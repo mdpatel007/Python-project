1aa1583de18d3a9f7bbf139416bd4ed2ab5c3c27
Mihir Dudhat <mihirdudhat123@gmail.com>
2025-07-20 11:04:47 +0530

    AUTO-REPLY AI CHATBOT

--- Python mega project-2/01_get_cursor.py
+++ Python mega project-2/01_get_cursor.py
@@ -3,6 +3,6 @@ import pyautogui
 while True:
     a = pyautogui.position()
     print(a)
-    ## This position is relative to the top-left corner of the screen.
+    # This position is relative to the top-left corner of the screen.
     # 587 , 139
     # 1872  , 915 to 1877 , 945
